f39240eee215df850ef19e782843345612e7b001
Andres Siles-Loayza <andres.siles-loayza@revature.net>
2020-10-20 17:10:43 -0400

    created Hero class in HeroesLib and referenced HeroesLib in HeroesUI

---
+++ HeroesApp/HeroesLib/Hero.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace HeroesLib
+{
+    #region old way of creating class members
+    /*public class Hero
+    {
+        private int id;
+        public int Id{
+            get{
+                return id;
+            }
+            set{
+                id=value;
+            }
+        }
+        public string name;
+
+        //default constructor
+        public Hero() {
+            this.id=1;
+            this.name="Bombasto";
+        }
+
+        //explicit constructor
+        public Hero(int id, string name)
+        {
+            this.id = id;
+            this.name = name;
+        }
+
+        public int getId() {
+            return this.id;
+        }
+    }*/
+    #endregion
+
+    #region modern way of creating class members
+    public class Hero {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string[] superPowers = new string[10];
+
+        public int[][] ja = new int[3][];
+    }
+    #endregion
+}
+
--- HeroesApp/HeroesUI/Program.cs
+++ HeroesApp/HeroesUI/Program.cs
@@ -1,12 +1,53 @@
 ﻿using System;
+using HeroesLib;
 
 namespace HeroesUI
 {
-    class Program
+    class Program:Hero
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Hero obj = new Hero();
+            #region default constructor
+            /*Hero obj = new Hero();
+            Console.WriteLine($"Hero ID: {obj.id} \nHero Name: {obj.name}");*/
+            #endregion
+            #region explicit constructor
+            /*obj.Id = 5;
+            Console.WriteLine($"Hero ID: {obj.Id} \nHero Name: {obj.name}");*/
+            #endregion
+            #region arrays
+            /*
+            Console.Write("Please enter your hero's id: ");
+            obj.Id=Int32.Parse(Console.ReadLine()); //typecast string to int
+            Console.Write("Please enter your Superhero name: ");
+            obj.Name=Console.ReadLine();
+            Console.Write("Please enter the first superpower: ");
+            obj.superPowers[0] = Console.ReadLine();
+            Console.Write($"ID: {obj.Id}\nName: {obj.Name}\nSuperpowers: {obj.superPowers[0]}");
+            */
+            #endregion
+
+            obj.ja[0] = new int[2];
+            obj.ja[1] = new int[3];
+            obj.ja[2] = new int[1];
+            obj.ja[0][0] =10;
+            obj.ja[1][2] =15;
+            int[,,] td = new int[2, 4, 3]; //3-D array
+            //Console.WriteLine(obj.ja.Rank); //dimension of the array (1D, 2D, etc.) (jagged arrays are techincally 1D)
+            //Console.WriteLine(obj.ja.Length);//maximum size of the array (num of elements)
+
+            Console.WriteLine("Values in Jagged Array:");
+            //loop through all rows
+            foreach (var row in obj.ja)
+            {
+                //loop through all columns
+                foreach (var val in row)
+                {
+                    Console.Write($"{val} ");
+                }
+                Console.Write("\n");
+            }
         }
     }
 }
