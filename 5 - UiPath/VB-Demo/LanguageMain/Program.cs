0bfca59b258ab4c257ca342a923ea6386e0fbc5d
Andres Siles-Loayza <andres.siles-loayza@revature.net>
2020-11-18 10:23:43 -0500

    reorganized files and folders

---
+++ 5 - UiPath/VB-Demo/LanguageMain/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace LanguageMain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CSharpLib.TestDemo testcsharp = new CSharpLib.TestDemo();
+            testcsharp.GetFullName("Andres", "", "Siles");
+            var resultsCsharp = testcsharp.LoadFile();
+            foreach (var item in resultsCsharp)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("*************************");
+            VbLib.TestDemo testvb = new VbLib.TestDemo(); //makes a reference to a VB class in C# code
+            testvb.GetFullName("Andres", "S", "Siles"); //language interactivity in .Net
+            var resultsVb = testvb.LoadFile();
+            foreach(var item in resultsVb)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
